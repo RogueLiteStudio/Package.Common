cf63d1891c1487e314a0c7dac99a242b516fa23e
张丹 <zh880517@163.com>
2024-01-14 22:21:22 +0800

    添加扩展函数库

---
+++ Runtime/Utils/CollectionUtil.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+public static class CollectionUtil
+{
+    public static bool IsEmpty<T>(this T[] array)
+    {
+        return array == null || array.Length == 0;
+    }
+
+    public static bool IsEmpty<T>(this List<T> list)
+    {
+        return list == null || list.Count == 0;
+    }
+}

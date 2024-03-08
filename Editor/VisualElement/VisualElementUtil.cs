8c17b0abe594bc011037c347ad8531b9a572c8ff
kyle <363130890@qq.com>
2024-03-08 19:18:08 +0800

    Update VisualElementUtil.cs

--- Editor/VisualElement/VisualElementUtil.cs
+++ Editor/VisualElement/VisualElementUtil.cs
@@ -3,21 +3,35 @@ using UnityEngine.UIElements;
 
 public static class VisualElementUtil
 {
-    public static void SetBorderColor(this VisualElement element, Color color)
+    public static VisualElement SetBorderColor(this VisualElement element, Color color)
     {
         var style = element.style;
         style.borderLeftColor = color;
         style.borderRightColor = color;
         style.borderTopColor = color;
         style.borderBottomColor = color;
+        return element;
     }
 
-    public static void SetBorderWidth(this VisualElement element, float width)
+    public static VisualElement SetBorderWidth(this VisualElement element, float width)
     {
         var style = element.style;
         style.borderLeftWidth = width;
         style.borderRightWidth = width;
         style.borderTopWidth = width;
         style.borderBottomWidth = width;
+        return element;
+    }
+
+    public static VisualElement SetTextAlign(this VisualElement element, TextAnchor anchor)
+    {
+        element.style.unityTextAlign = anchor;
+        return element;
+    }
+
+    public static VisualElement SetTextStyle(this VisualElement element, FontStyle style)
+    {
+        element.style.unityFontStyleAndWeight = style;
+        return element;
     }
 }

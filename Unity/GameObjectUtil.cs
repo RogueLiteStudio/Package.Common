7c012791e9fa8491f932b3a8d96981b16e021768
kyle <363130890@qq.com>
2024-04-04 19:27:39 +0800

    添加辅助工具库

---
+++ Unity/GameObjectUtil.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public static class GameObjectUtil
+{
+    public static Transform RecursiveFindChild(this Transform root, string name, bool ignoreCase = false)
+    {
+        if (!root)
+            return null;
+        for (int i=0; i<root.childCount; ++i)
+        {
+            var child = root.GetChild(i);
+            if (ignoreCase)
+            {
+                if (child.name.Equals(name, System.StringComparison.OrdinalIgnoreCase))
+                    return child;
+            }
+            else
+            {
+                if (child.name == name)
+                    return child;
+            }
+            var find = RecursiveFindChild(child, name, ignoreCase);
+            if (find)
+                return find;
+        }
+        return null;
+    }
+
+    public static Transform RecursiveFindChild(this GameObject root, string name, bool ignoreCase = false)
+    {
+        return RecursiveFindChild(root.transform, name, false);
+    }
+}

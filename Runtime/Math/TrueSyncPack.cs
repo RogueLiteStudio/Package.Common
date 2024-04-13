e2fb509e4664956490b6e50eb2e506fc90ce231b
张丹 <zh880517@163.com>
2024-04-13 13:06:54 +0800

    Update TrueSyncPack.cs

--- Runtime/Math/TrueSyncPack.cs
+++ Runtime/Math/TrueSyncPack.cs
@@ -63,4 +63,30 @@ public class TrueSyncPack : SdpLitePacker
         else
             packer.PackHeader(tag, SdpLite.DataType.StructEnd);
     }
+    public static void Pack(SdpLite.Packer packer, uint tag, bool require, TVector3Int value)
+    {
+        var positoin0 = packer.Position;
+        packer.PackHeader(tag, SdpLite.DataType.StructBegin);
+        var prePositoin = packer.Position;
+        Pack(packer, 1, false, value.x);
+        Pack(packer, 2, false, value.y);
+        Pack(packer, 3, false, value.z);
+        if (packer.Position == prePositoin && !require)
+            packer.Rewind(positoin0);
+        else
+            packer.PackHeader(tag, SdpLite.DataType.StructEnd);
+    }
+
+    public static void Pack(SdpLite.Packer packer, uint tag, bool require, TVector2Int value)
+    {
+        var positoin0 = packer.Position;
+        packer.PackHeader(tag, SdpLite.DataType.StructBegin);
+        var prePositoin = packer.Position;
+        Pack(packer, 1, false, value.x);
+        Pack(packer, 2, false, value.y);
+        if (packer.Position == prePositoin && !require)
+            packer.Rewind(positoin0);
+        else
+            packer.PackHeader(tag, SdpLite.DataType.StructEnd);
+    }
 }
